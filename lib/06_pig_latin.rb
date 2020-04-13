06d88782bb658468faa2eaeefe5fe80162809fb9
Eva Afriana <vava.afriana@gmail.com>
2020-04-13 18:43:39 +0200

    update pig latin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -6,9 +6,15 @@ end
 def trans_word(word)
   if word.match(/^[aiueo]/)
     word + "ay"
-  elsif word[1].match(/[aieo]/)
+  elsif word.match(/^qu/)
+    word[2...word.length] + "quay"
+  elsif word.match(/^.?qu/)
+    word[3...word.length] + word[0] + "quay"
+  elsif word[1].match(/[aiueo]/)
     word[1...word.length] + word[0] + "ay"
-  else
+  elsif word[2].match(/[aieuo]/)
     word[2...word.length] + word[0..1] + "ay"
+  else
+    word[3...word.length] + word[0..2] + "ay"
   end
 end
