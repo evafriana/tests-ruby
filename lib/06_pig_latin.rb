8f764017b9e67fea26ca33e09b341c9b8d792860
Eva Afriana <vava.afriana@gmail.com>
2020-04-14 13:14:13 +0200

    update pig latin

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -1,20 +1,19 @@
 def translate(words)
-  words.split.map { |word| trans_word(word) }.join(" ")
-
+  words.split.map { |word| trans_word(word) }.join(' ')
 end
 
 def trans_word(word)
   if word.match(/^[aiueo]/)
-    word + "ay"
+    word + 'ay'
   elsif word.match(/^qu/)
-    word[2...word.length] + "quay"
+    word[2...word.length] + 'quay'
   elsif word.match(/^.?qu/)
-    word[3...word.length] + word[0] + "quay"
+    word[3...word.length] + word[0] + 'quay'
   elsif word[1].match(/[aiueo]/)
-    word[1...word.length] + word[0] + "ay"
+    word[1...word.length] + word[0] + 'ay'
   elsif word[2].match(/[aieuo]/)
-    word[2...word.length] + word[0..1] + "ay"
+    word[2...word.length] + word[0..1] + 'ay'
   else
-    word[3...word.length] + word[0..2] + "ay"
+    word[3...word.length] + word[0..2] + 'ay'
   end
 end
